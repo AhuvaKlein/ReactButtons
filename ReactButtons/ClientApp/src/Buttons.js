16b6a0989055017689f68155a84030cebc63419b
Ahuva Klein <ahuvalklein@gmail.com>
2019-05-28 23:49:06 -0400

    Commit 1

---
+++ ReactButtons/ClientApp/src/Buttons.js
@@ -0,0 +1,55 @@
+﻿import React from 'react';
+import randomColor from 'randomcolor';
+
+class Buttons extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+            incrementBy: 1,
+            color: randomColor()
+        };
+
+    }
+
+
+    increment = () => {
+        this.setState({ counter: this.state.counter + this.state.incrementBy })
+    }
+
+    incrementBy = () => {
+        this.setState({ incrementBy: this.state.incrementBy + 1 })
+    }
+
+    changeColor = () => {
+        this.setState({ color: randomColor() });
+    }
+
+    render() {
+
+        const colorStyle = {
+            color: this.state.color
+        };
+
+        return (
+            <div className='row'>
+                <div className='col-md-2 col-md-offset-5 well'>
+                    <h2 style={colorStyle}> {this.state.counter}</h2>
+                    <button onClick={this.increment} className="btn btn-danger">Increment</button>
+                </div>
+                <div className='col-md-2 col-md-offset-5 well'>
+                    <h2>{this.state.incrementBy}</h2>
+                    <button onClick={this.incrementBy} className="btn btn-primary">Increment By</button>
+                </div>
+                <div className='col-md-2 col-md-offset-5 well'>
+                    <button onClick={this.changeColor} className="btn btn-warning">Change Color</button>
+                </div>
+            </div >
+
+        )
+    }
+
+
+}
+export default Buttons;
